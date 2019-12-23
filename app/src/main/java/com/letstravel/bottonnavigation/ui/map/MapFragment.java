20be5c4d72a5d52e3cf31c9a83b45e02a45a2a87
rmfadera10 <55060246+rmfadera10@users.noreply.github.com>
2019-12-23 14:11:48 +0545

    first branch

---
+++ app/src/main/java/com/letstravel/bottonnavigation/ui/map/MapFragment.java
@@ -0,0 +1,32 @@
+package com.letstravel.bottonnavigation.ui.map;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.letstravel.bottonnavigation.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapFragment extends Fragment {
+
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/letstravel/bottonnavigation/ui/mobile/Mobile.java
@@ -0,0 +1,31 @@
+package com.letstravel.bottonnavigation.ui.mobile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.letstravel.bottonnavigation.R;
+
+public class Mobile extends Fragment {
+    private WebView webView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        webView = (WebView) root.findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://softwarica.edu.np/");
+        return root;
+    }
+}
+
+
+
